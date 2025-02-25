b012d5f06a51ed7f5f92be13f06172fc24ecfc0e
AdityaAnandMishra <48655608+AdityaAnandMishra@users.noreply.github.com>
2025-02-25 09:54:57 +0530

    option

--- Password_mail/src/Password.jsx
+++ Password_mail/src/Password.jsx
@@ -3,8 +3,8 @@ import {useState} from "react";
 function Password() {
     const [password, setPassword] = useState('')
     const [length, setLength] = useState('8')
-    const [isNumber, setIsNumber] = useState('false')
-    const [isCharacter, setIsCharacter] = useState('false')
+    const [isNumber, setIsNumber] = useState(false)
+    const [isCharacter, setIsCharacter] = useState(false)
 
     return(
         <>
@@ -27,7 +27,7 @@ function Password() {
                         >Copy
                         </button>
                     </div>
-                    <h1 className="text-base/7 font-semibold dark:text-white">Customization option</h1>
+                    <h1 className="text-base/7 font-semibold dark:text-white p-5">Customization option</h1>
                     <div className='flex flex-col text-sm gap-x-2'>
                         <div className='flex items-center mb-4'>
                             <input id="steps-range" type="range" min="6" max="50" value={length}
@@ -36,16 +36,16 @@ function Password() {
                             <label className="flex-none ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Length: {length}</label>
                         </div>
                         <div className="flex items-center mb-4">
-                            <input id="default-checkbox" type="checkbox" value=""
-                                   className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
-                            <label className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Default
-                                checkbox</label>
+                            <input id="default-checkbox" type="checkbox" defaultChecked={isNumber}
+                                   className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                                    onChange={()=>{setIsNumber((prev) => !prev) }}/>
+                            <label className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Number</label>
                         </div>
                         <div className="flex items-center mb-4">
-                            <input id="default-checkbox" type="checkbox" value=""
-                                   className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
-                            <label className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Default
-                                checkbox</label>
+                            <input id="default-checkbox" type="checkbox" defaultChecked={isCharacter}
+                                   className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                                   onChange={()=>{setIsCharacter((prev) => !prev) }}/>
+                            <label className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Character</label>
                         </div>
                     </div>
                 </div>
